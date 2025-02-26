8a9c5b69fee473f07469487b6fab049c63c64781
Prajwal <92498705+Sallapalli-Prajwal@users.noreply.github.com>
2025-02-26 04:29:11 +0000

    added code to retrieve sales data

--- Scripts/01-03.sql
+++ Scripts/01-03.sql
@@ -1,20 +1,33 @@
 -- List all customers & their sales, even if some data is gone
 
-SELECT cus.firstName, cus.lastName, cus.email, sls.salesAmount, sls.soldDate
-FROM customer cus
-INNER JOIN sales sls
-    ON cus.customerId = sls.customerId
-UNION
--- UNION WITH CUSTOMERS WHO HAVE NO SALES
-SELECT cus.firstName, cus.lastName, cus.email, sls.salesAmount, sls.soldDate
-FROM customer cus
-LEFT JOIN sales sls
-    ON cus.customerId = sls.customerId
-WHERE sls.salesId IS NULL
-UNION
--- UNION WITH SALES MISSING CUSTOMER DATA
-SELECT cus.firstName, cus.lastName, cus.email, sls.salesAmount, sls.soldDate
-FROM sales sls
-LEFT JOIN customer cus
-    ON cus.customerId = sls.customerId
-WHERE cus.customerId IS NULL;
+SELECT c.firstName,c.lastName,c.email,s.salesAmount,s.soldDate
+FROM customer c LEFT JOIN sales s ON c.customerID = s.customerID
+ UNION
+SELECT c.firstName,c.lastName,c.email,s.salesAmount,s.soldDate
+FROM sales s LEFT JOIN customer c ON c.customerID = s.customerID WHERE c.firstName IS NULL;
+
+
+
+
+
+
+
+
+-- SELECT cus.firstName, cus.lastName, cus.email, sls.salesAmount, sls.soldDate
+-- FROM customer cus
+-- INNER JOIN sales sls
+--     ON cus.customerId = sls.customerId
+-- UNION
+-- -- UNION WITH CUSTOMERS WHO HAVE NO SALES
+-- SELECT cus.firstName, cus.lastName, cus.email, sls.salesAmount, sls.soldDate
+-- FROM customer cus
+-- LEFT JOIN sales sls
+--     ON cus.customerId = sls.customerId
+-- WHERE sls.salesId IS NULL
+-- UNION
+-- -- UNION WITH SALES MISSING CUSTOMER DATA
+-- SELECT cus.firstName, cus.lastName, cus.email, sls.salesAmount, sls.soldDate
+-- FROM sales sls
+-- LEFT JOIN customer cus
+--     ON cus.customerId = sls.customerId
+-- WHERE cus.customerId IS NULL;

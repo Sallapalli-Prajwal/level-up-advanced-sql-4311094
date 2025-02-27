4a87cbfda103cb8faf4561cbe8084521af49784c
Prajwal <92498705+Sallapalli-Prajwal@users.noreply.github.com>
2025-02-27 02:42:40 +0000

    added code for number of cars sold per employee

--- Scripts/02-01.sql
+++ Scripts/02-01.sql
@@ -1,12 +1,18 @@
 -- How many cars has been sold per employee
 
--- start with this query
-SELECT emp.employeeId, emp.firstName, emp.lastName
-FROM sales sls
-INNER JOIN employee emp
-    ON sls.employeeId = emp.employeeId
+-- SELECT e.firstName,COUNT(s.salesID) AS cars_sold
+-- FROM employee e Left JOIN sales s ON e.employeeId = s.employeeId
+-- GROUP BY e.firstName
+-- ORDER BY cars_sold DESC;
 
--- then add the group by & count
+SELECT e.employeeId,e.firstName,e.lastName , count(*) as cars_sold
+FROM sales s 
+INNER JOIN employee e 
+    on s.employeeId = e.employeeId
+GROUP BY e.employeeId,e.firstName,e.lastName
+ORDER BY cars_sold DESC;
+
+-- -- then add the group by & count
 SELECT emp.employeeId, emp.firstName, emp.lastName, count(*) as NumOfCarsSold
 FROM sales sls
 INNER JOIN employee emp

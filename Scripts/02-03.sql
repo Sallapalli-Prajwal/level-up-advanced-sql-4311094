af7d5913ab40614e80b249558595902cf8f0de5e
Prajwal <92498705+Sallapalli-Prajwal@users.noreply.github.com>
2025-03-03 20:46:58 +0000

    general

--- Scripts/02-03.sql
+++ Scripts/02-03.sql
@@ -1,12 +1,36 @@
 -- Display report for employees who have sold at least 5 cars
 
-SELECT emp.employeeId, 
-    count(*) AS NumOfCarsSold, 
-    MIN(salesAmount) AS MinSalesAmount, 
-    MAX(salesAmount) AS MaxSalesAmount
-FROM sales sls
-INNER JOIN employee emp
-    ON sls.employeeId = emp.employeeId
-WHERE sls.soldDate >= date('now','start of year')
-GROUP BY emp.employeeId
-HAVING count(*) > 5
+
+SELECT e.employeeID,e.firstName,
+        count(*) as numOfCarsSold
+FROM sales s INNER JOIN employee e
+     on e.employeeID = s.employeeId
+group BY e.employeeID,e.firstName
+  having count(*)>5;
+
+
+-- total sales by each employee;
+
+SELECT e.employeeID,e.firstName,
+        sum(s.salesAmount) as totalSales
+FROM sales s INNER JOIN employee e
+     on e.employeeID = s.employeeId
+group BY e.employeeID,e.firstName
+ -- having count(*)>5;
+
+
+
+
+
+
+
+-- SELECT emp.employeeId, 
+--     count(*) AS NumOfCarsSold, 
+--     MIN(salesAmount) AS MinSalesAmount, 
+--     MAX(salesAmount) AS MaxSalesAmount
+-- FROM sales sls
+-- INNER JOIN employee emp
+--     ON sls.employeeId = emp.employeeId
+-- WHERE sls.soldDate >= date('now','start of year')
+-- GROUP BY emp.employeeId
+-- HAVING count(*) > 5
